dadb1fd86fcd129c639fd6dfc528004689ed152c
Tyler Cadena <tylercadena2@gmail.com>
2020-07-31 12:22:05 -0700

    windows fixed

--- cmake/FindTmxlite.cmake
+++ cmake/FindTmxlite.cmake
@@ -1,7 +1,12 @@
 include (${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 
 find_path (TMXLITE_INCLUDE_DIR NAMES tmxlite/Config.hpp PATH_SUFFIXES include)
-find_library (TMXLITE_LIBRARY NAMES tmxlite PATH_SUFFIXES lib)
+
+if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+	find_library (TMXLITE_LIBRARY NAMES tmxlite-d PATH_SUFFIXES debug/lib)
+else ()
+	find_library (TMXLITE_LIBRARY NAMES tmxlite PATH_SUFFIXES lib)
+endif ()
 
 find_package_handle_standard_args (
     TMXLITE
