e621767b53ee73f0c76a237e9070c8bb8640d21d
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-08-22 13:29:31 -0700

    precompiled headers

--- source/stdafx.hpp
+++ source/stdafx.hpp
@@ -7,11 +7,16 @@
 #include <cstdint>
 #include <cinttypes>
 #include <cassert>
+#include <cctype>
 #include <cstring>
 #include <cstdio>
+#include <clocale>
+#include <cmath>
+#include <cfloat>
 
 // C++ standard libraries
 #include <typeinfo>
+#include <type_traits>
 #include <bitset>
 #include <functional>
 #include <utility>
@@ -28,10 +33,12 @@
 #include <set>
 #include <map>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
 #include <algorithm>
 #include <random>
 #include <numeric>
+#include <iosfwd>
 #include <fstream>
 #include <sstream>
 #include <iomanip>
@@ -41,21 +48,42 @@
 #include <thread>
 #include <mutex>
 #include <future>
-
-#include "./types.hpp"
-
-#ifdef LEVIATHAN_TOOLCHAIN_APPLECLANG
-	#include <filesystem>
-#endif // LEVIATHAN_TOOLCHAIN_APPLECLANG
-
-// Other libraries
 #include <glm/glm.hpp>
+#include <glm/common.hpp>
 #include <glm/gtc/constants.hpp>
-#include <entt/entt.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/geometric.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/matrix.hpp>
+#include <glm/gtc/vec1.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <entt/entity/registry.hpp>
 #include <tmxlite/Map.hpp>
 #include <tmxlite/TileLayer.hpp>
+#include <tmxlite/ObjectGroup.hpp>
 #include <angelscript.h>
 #include <nlohmann/json.hpp>
 #include <SDL2/SDL.h>
+#include <glad/glad.hpp>
 
-#endif // LEVIATHAN_INCLUDED_STDAFX_HPP
+#ifdef LEVIATHAN_TOOLCHAIN_APPLECLANG
+	#include <filesystem>
+#endif
+
+#ifdef LEVIATHAN_USES_VCPKG
+	#include <al.h>
+	#include <alc.h>
+#else
+	#include <AL/al.h>
+	#include <AL/alc.h>
+#endif
+
+#include "./types.hpp"
+#include "./utility/setup_file.hpp"
+#include "./resource/icon.hpp"
+#include "./pxtone/pxtnService.h"
+#include "./menu/widget.hpp"
+
+#endif
