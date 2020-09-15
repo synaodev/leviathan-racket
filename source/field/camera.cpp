1faef72d7332f98dbd0550a86b653fa0c9d6941d
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-09-15 03:18:15 -0700

    camera offset bugfix

--- source/field/camera.cpp
+++ source/field/camera.cpp
@@ -88,8 +88,8 @@ void camera_t::update(real64_t delta) {
 }
 
 void camera_t::set_view_limits(rect_t view_limits) {
-	this->view_limits.w = view_limits.w - constants::TileSize<real_t>();
-	this->view_limits.h = view_limits.h - constants::TileSize<real_t>() - 4.0f;
+	this->view_limits.w = view_limits.w - 16.0f;
+	this->view_limits.h = view_limits.h - 12.0f;
 	position = kDefaultCenter;
 	dimensions = constants::NormalDimensions<real_t>();
 }
@@ -145,18 +145,19 @@ arch_t camera_t::get_tile_range(glm::ivec2 first, glm::ivec2 last) const {
 }
 
 glm::mat4 camera_t::get_matrix() const {
-	glm::mat4 result = glm::scale(
-		glm::mat4(1.0f), glm::vec3(2.0f / dimensions.x, 2.0f / -dimensions.y, 0.0f)
+	glm::mat4 matrix = glm::scale(
+		glm::mat4(1.0f),
+		glm::vec3(2.0f / dimensions.x, 2.0f / -dimensions.y, 0.0f)
 	);
 	if (view_angle != 0.0f) {
-		result = glm::rotate(
-			result,
+		matrix = glm::rotate(
+			matrix,
 			view_angle,
 			glm::vec3(0.0f, 0.0f, 1.0f)
 		);
 	}
-	result = glm::translate(
-		result, -glm::vec3(glm::round(position + offsets), 0.0f)
+	matrix = glm::translate(
+		matrix, -glm::vec3(glm::round(position + offsets), 0.0f)
 	);
-	return result;
+	return matrix;
 }
