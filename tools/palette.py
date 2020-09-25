d96649a3c87fd9aafdfba03c0a020119c53f540b
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-09-25 02:29:43 -0700

    color limits

--- tools/palette.py
+++ tools/palette.py
@@ -9,15 +9,23 @@ def get_nearest_power_of_two(value: int) -> int:
         result <<= 1
     return result
 
-def get_colors_from_pixels(pixels: list) -> list:
+def get_colors_from_pixels(pixels: list, limit: int) -> list:
     colors: list = []
     for pix in pixels:
-        if colors.count(pix) == 0:
+        count: int = colors.count(pix)
+        if limit > 0 and count >= limit:
+            print(f'Color count is above limit of {limit}!')
+            break
+        if count == 0:
             colors.append(pix)
     return colors
 
-def get_palette_from_colors(colors: list) -> Image:
-    length: int = get_nearest_power_of_two(len(colors))
+def get_palette_from_colors(colors: list, limit: int) -> Image:
+    length: int = 0
+    if limit > 0:
+        length = limit
+    else:
+        length: int = get_nearest_power_of_two(len(colors))
     image: Image = Image.new('RGBA', (length, 1), 0)
     for i in range(len(colors)):
         image.putpixel(
@@ -26,8 +34,12 @@ def get_palette_from_colors(colors: list) -> Image:
         )
     return image
 
-def get_indexed_from_colors(pixels: list, colors: list, size: tuple) -> Image:
-    length: int = get_nearest_power_of_two(len(colors))
+def get_indexed_from_colors(pixels: list, colors: list, size: tuple, limit: int) -> Image:
+    length: int = 0
+    if limit > 0:
+        length = limit
+    else:
+        length: int = get_nearest_power_of_two(len(colors))
     image: Image = Image.new('RGBA', size, 0)
     for x in range(size[0]):
         for y in range(size[1]):
@@ -46,26 +58,48 @@ def get_indexed_from_colors(pixels: list, colors: list, size: tuple) -> Image:
                 )
     return image
 
-def make_palette_image(initial_path: str, colors: list):
-    palette_image: Image = get_palette_from_colors(colors)
+def make_palette_image(initial_path: str, colors: list, limit: int):
+    palette_image: Image = get_palette_from_colors(colors, limit)
     palette_path: str = os.path.splitext(initial_path)[0] + '_palette.png'
     palette_image.save(palette_path)
 
-def make_indexed_image(initial_path: str, pixels: list, colors: list, size: tuple):
-    indexed_image: Image = get_indexed_from_colors(pixels, colors, size)
+def make_indexed_image(initial_path: str, pixels: list, colors: list, size: tuple, limit: int):
+    indexed_image: Image = get_indexed_from_colors(pixels, colors, size, limit)
     indexed_path: str = os.path.splitext(initial_path)[0] + '_indexed.png'
     indexed_image.save(indexed_path)
+
+def get_limit_type(arg: str) -> int:
+    if arg.endswith('64'):
+        return 64
+    if arg.endswith('32'):
+        return 32
+    if arg.endswith('16'):
+        return 16
+    if arg.endswith('8'):
+        return 8
+    if arg.endswith('4'):
+        return 4
+    if arg.endswith('2'):
+        return 2
+    return 0
 
 def main():
     argc: int = len(sys.argv)
     if argc > 1:
+        limit: int = 0
         for i in range(1, argc):
-            initial_path: str = sys.argv[i]
-            initial_image: None = Image.open(initial_path)
-            pixel_list: list = list(initial_image.getdata())
-            color_list: list = get_colors_from_pixels(pixel_list)
-            make_palette_image(initial_path, color_list)
-            make_indexed_image(initial_path, pixel_list, color_list, initial_image.size)
+            if sys.argv[i].startswith('--limit='):
+                if limit == 0:
+                    limit = get_limit_type(sys.argv[i])
+                else:
+                    print('Don\'t set limit more than once!')
+            else:
+                initial_path: str = sys.argv[i]
+                initial_image: None = Image.open(initial_path)
+                pixel_list: list = list(initial_image.getdata())
+                color_list: list = get_colors_from_pixels(pixel_list, limit)
+                make_palette_image(initial_path, color_list, limit)
+                make_indexed_image(initial_path, pixel_list, color_list, initial_image.size, limit)
     else:
         print('Error! No image paths given!')
 
