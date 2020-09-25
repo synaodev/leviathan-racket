5fd2aa86240899d5dcbaddeb90fcfafc5376403a
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-09-25 00:57:43 -0700

    editorconfig change

--- tools/config.py
+++ tools/config.py
@@ -3,73 +3,73 @@
 import os, sys
 
 def main():
-	if len(sys.argv) > 3:
-		print('Error! Too many arguments!')
-		return
-	force_generate: bool = False
-	into_directory: str = ''
-	for i in range(0, len(sys.argv)):
-		if force_generate and len(into_directory) > 0:
-			break
-		elif sys.argv[i] == '--force':
-			force_generate = True
-		elif i > 1 and len(sys.argv[i]) != 0:
-			into_directory = sys.argv[i]
-	config_path: str = os.path.abspath(os.path.join(os.path.curdir, os.path.pardir, 'vfs', 'init'))
-	if len(into_directory) != 0:
-		config_path = os.path.join(config_path, into_directory)
-	config_file: str = os.path.join(config_path, 'boot.cfg')
-	if os.path.exists(config_file) and not force_generate:
-		if os.path.isfile(config_file):
-			print('Configuration file already exists! Run this script again with \'--force\' to regenerate the configuration file.')
-		else:
-			print(f'Error! There is already a filesystem object at {config_file} which is not a file! Please remove/rename it try again...')
-		return
-	elif os.path.exists(config_file) and force_generate:
-		os.remove(config_file)
-	if not os.path.exists(config_path):
-		os.mkdir(config_path, 493) # 0775
-	with open(config_file, 'x') as file:
-		file.write('\n[Setup]\n\n')
-		file.write('MetaMenu = 0\n')
-		file.write('OpenGL4X = 1\n')
-		file.write('Language = english\n')
-		file.write('\n[Video]\n\n')
-		file.write('VerticalSync = 1\n')
-		file.write('Fullscreen = 0\n')
-		file.write('ScaleFactor = 3\n')
-		file.write('FrameLimiter = 60\n')
-		file.write('UseOpenGL4 = 1\n')
-		file.write('\n[Audio]\n\n')
-		file.write('Volume = 1\n')
-		file.write('\n[Music]\n\n')
-		file.write('Volume = 0.34\n')
-		file.write('Channels = 2\n')
-		file.write('SamplingRate = 44100\n')
-		file.write('kBufferedTime = 0.1\n')
-		file.write('\n[Input]\n\n')
-		file.write('KeyJump = 29\n')
-		file.write('KeyHammer = 27\n')
-		file.write('KeyItem = 225\n')
-		file.write('KeyLiteDash = 4\n')
-		file.write('KeyContext = 44\n')
-		file.write('KeyStrafe = 224\n')
-		file.write('KeyInventory = 43\n')
-		file.write('KeyOptions = 41\n')
-		file.write('KeyUp = 82\n')
-		file.write('KeyDown = 81\n')
-		file.write('KeyLeft = 80\n')
-		file.write('KeyRight = 79\n')
-		file.write('JoyJump = 0\n')
-		file.write('JoyHammer = 1\n')
-		file.write('JoyItem = 2\n')
-		file.write('JoyLiteDash = 3\n')
-		file.write('JoyContext = 4\n')
-		file.write('JoyStrafe = 5\n')
-		file.write('JoyInventory = 6\n')
-		file.write('JoyOptions = 7\n')
-		file.close()
-	print(f'Successfully created file {config_file}!')
+    if len(sys.argv) > 3:
+        print('Error! Too many arguments!')
+        return
+    force_generate: bool = False
+    into_directory: str = ''
+    for i in range(0, len(sys.argv)):
+        if force_generate and len(into_directory) > 0:
+            break
+        elif sys.argv[i] == '--force':
+            force_generate = True
+        elif i > 1 and len(sys.argv[i]) != 0:
+            into_directory = sys.argv[i]
+    config_path: str = os.path.abspath(os.path.join(os.path.curdir, os.path.pardir, 'vfs', 'init'))
+    if len(into_directory) != 0:
+        config_path = os.path.join(config_path, into_directory)
+    config_file: str = os.path.join(config_path, 'boot.cfg')
+    if os.path.exists(config_file) and not force_generate:
+        if os.path.isfile(config_file):
+            print('Configuration file already exists! Run this script again with \'--force\' to regenerate the configuration file.')
+        else:
+            print(f'Error! There is already a filesystem object at {config_file} which is not a file! Please remove/rename it try again...')
+        return
+    elif os.path.exists(config_file) and force_generate:
+        os.remove(config_file)
+    if not os.path.exists(config_path):
+        os.mkdir(config_path, 493) # 0775
+    with open(config_file, 'x') as file:
+        file.write('\n[Setup]\n\n')
+        file.write('MetaMenu = 0\n')
+        file.write('OpenGL4X = 1\n')
+        file.write('Language = english\n')
+        file.write('\n[Video]\n\n')
+        file.write('VerticalSync = 1\n')
+        file.write('Fullscreen = 0\n')
+        file.write('ScaleFactor = 3\n')
+        file.write('FrameLimiter = 60\n')
+        file.write('UseOpenGL4 = 1\n')
+        file.write('\n[Audio]\n\n')
+        file.write('Volume = 1\n')
+        file.write('\n[Music]\n\n')
+        file.write('Volume = 0.34\n')
+        file.write('Channels = 2\n')
+        file.write('SamplingRate = 44100\n')
+        file.write('kBufferedTime = 0.1\n')
+        file.write('\n[Input]\n\n')
+        file.write('KeyJump = 29\n')
+        file.write('KeyHammer = 27\n')
+        file.write('KeyItem = 225\n')
+        file.write('KeyLiteDash = 4\n')
+        file.write('KeyContext = 44\n')
+        file.write('KeyStrafe = 224\n')
+        file.write('KeyInventory = 43\n')
+        file.write('KeyOptions = 41\n')
+        file.write('KeyUp = 82\n')
+        file.write('KeyDown = 81\n')
+        file.write('KeyLeft = 80\n')
+        file.write('KeyRight = 79\n')
+        file.write('JoyJump = 0\n')
+        file.write('JoyHammer = 1\n')
+        file.write('JoyItem = 2\n')
+        file.write('JoyLiteDash = 3\n')
+        file.write('JoyContext = 4\n')
+        file.write('JoyStrafe = 5\n')
+        file.write('JoyInventory = 6\n')
+        file.write('JoyOptions = 7\n')
+        file.close()
+    print(f'Successfully created file {config_file}!')
 
 if __name__ == '__main__':
-	main()
+    main()
--- tools/palette.py
+++ tools/palette.py
@@ -4,70 +4,70 @@ from PIL import Image
 import os, sys, numpy
 
 def get_nearest_power_of_two(value: int) -> int:
-	result: int = 1
-	while result < value:
-		result <<= 1
-	return result
+    result: int = 1
+    while result < value:
+        result <<= 1
+    return result
 
 def get_colors_from_pixels(pixels: list) -> list:
-	colors: list = []
-	for pix in pixels:
-		if colors.count(pix) == 0:
-			colors.append(pix)
-	return colors
+    colors: list = []
+    for pix in pixels:
+        if colors.count(pix) == 0:
+            colors.append(pix)
+    return colors
 
 def get_palette_from_colors(colors: list) -> Image:
-	length: int = get_nearest_power_of_two(len(colors))
-	image: Image = Image.new('RGBA', (length, 1), 0)
-	for i in range(len(colors)):
-		image.putpixel(
-			(i, 0), 
-			colors[i]
-		)
-	return image
+    length: int = get_nearest_power_of_two(len(colors))
+    image: Image = Image.new('RGBA', (length, 1), 0)
+    for i in range(len(colors)):
+        image.putpixel(
+            (i, 0),
+            colors[i]
+        )
+    return image
 
 def get_indexed_from_colors(pixels: list, colors: list, size: tuple) -> Image:
-	length: int = get_nearest_power_of_two(len(colors))
-	image: Image = Image.new('RGBA', size, 0)
-	for x in range(size[0]):
-		for y in range(size[1]):
-			index: int = colors.index(pixels[y * size[0] + x])
-			if index != 0:
-				ratio: float = float(index) / float(length)
-				value: int = int(255.0 * ratio) + 1
-				image.putpixel(
-					(x, y),
-					(value, value, value, 255)
-				)
-			else:
-				image.putpixel(
-					(x, y), 
-					(0, 0, 0, 0)
-				)
-	return image
+    length: int = get_nearest_power_of_two(len(colors))
+    image: Image = Image.new('RGBA', size, 0)
+    for x in range(size[0]):
+        for y in range(size[1]):
+            index: int = colors.index(pixels[y * size[0] + x])
+            if index != 0:
+                ratio: float = float(index) / float(length)
+                value: int = int(255.0 * ratio) + 1
+                image.putpixel(
+                    (x, y),
+                    (value, value, value, 255)
+                )
+            else:
+                image.putpixel(
+                    (x, y),
+                    (0, 0, 0, 0)
+                )
+    return image
 
 def make_palette_image(initial_path: str, colors: list):
-	palette_image: Image = get_palette_from_colors(colors)
-	palette_path: str = os.path.splitext(initial_path)[0] + '_palette.png'
-	palette_image.save(palette_path)
+    palette_image: Image = get_palette_from_colors(colors)
+    palette_path: str = os.path.splitext(initial_path)[0] + '_palette.png'
+    palette_image.save(palette_path)
 
 def make_indexed_image(initial_path: str, pixels: list, colors: list, size: tuple):
-	indexed_image: Image = get_indexed_from_colors(pixels, colors, size)
-	indexed_path: str = os.path.splitext(initial_path)[0] + '_indexed.png'
-	indexed_image.save(indexed_path)
+    indexed_image: Image = get_indexed_from_colors(pixels, colors, size)
+    indexed_path: str = os.path.splitext(initial_path)[0] + '_indexed.png'
+    indexed_image.save(indexed_path)
 
 def main():
-	argc: int = len(sys.argv)
-	if argc > 1:
-		for i in range(1, argc):
-			initial_path: str = sys.argv[i]
-			initial_image: None = Image.open(initial_path)
-			pixel_list: list = list(initial_image.getdata())
-			color_list: list = get_colors_from_pixels(pixel_list)
-			make_palette_image(initial_path, color_list)
-			make_indexed_image(initial_path, pixel_list, color_list, initial_image.size)
-	else:
-		print('Error! No image paths given!')
+    argc: int = len(sys.argv)
+    if argc > 1:
+        for i in range(1, argc):
+            initial_path: str = sys.argv[i]
+            initial_image: None = Image.open(initial_path)
+            pixel_list: list = list(initial_image.getdata())
+            color_list: list = get_colors_from_pixels(pixel_list)
+            make_palette_image(initial_path, color_list)
+            make_indexed_image(initial_path, pixel_list, color_list, initial_image.size)
+    else:
+        print('Error! No image paths given!')
 
 if __name__ == '__main__':
-	main()
+    main()
