000e89886008de70fbe4ba173f0ad095910ab967
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-10-07 16:27:48 -0700

    changed tools arrangement and removed config.py

---
+++ tool-attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+application = QApplication([])
+window = QMainWindow()
+
+if __name__ == '__main__':
+    window.show()
+    application.exec()
