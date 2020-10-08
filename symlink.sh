1bc244e3046a840020a300865b575007b9d66fca
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-10-07 21:58:48 -0700

    added symlink file for ease of use

---
+++ symlink.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "${XDG_DATA_HOME}" ]; then
+	echo "XDG_DATA_HOME variable is not defined!"
+	exit 1
+fi
+
+if [ ! -d "data" ]; then
+	echo "Can't find data folder!"
+	exit 1
+fi
+
+cd "data"
+
+if [ ! -L "init" ]; then
+	ln -s "${XDG_DATA_HOME}/studio-synao/leviathan/init" "init"
+fi
+
+if [ ! -L "save" ]; then
+	ln -s "${XDG_DATA_HOME}/studio-synao/leviathan/save" "save"
+fi
+
+cd ".."
+
+exit 0
