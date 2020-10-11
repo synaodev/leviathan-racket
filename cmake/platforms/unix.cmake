ed0929789f0017b14b852cdc4df84f0490e58c79
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-10-11 04:14:48 -0700

    simplified cmake args

--- cmake/platforms/unix.cmake
+++ cmake/platforms/unix.cmake
@@ -14,9 +14,6 @@ if (NAOMI_BUILD)
 		${STLFS_LIBS}
 		${CMAKE_DL_LIBS}
 	)
-	if (NOT RTTI_BUILD)
-		target_compile_options (naomi PRIVATE "-fno-rtti")
-	endif ()
 endif ()
 if (EDITOR_BUILD)
 	target_link_libraries (editor PRIVATE
@@ -24,19 +21,4 @@ if (EDITOR_BUILD)
 		${STLFS_LIBS}
 		${CMAKE_DL_LIBS}
 	)
-	if (NOT RTTI_BUILD)
-		target_compile_options (editor PRIVATE "-fno-rtti")
-	endif ()
-endif ()
-
-if (GOLD_BUILD)
-	if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang" AND NOT LLVM_USE_LINKER)
-		message ("Warning! Compiler is clang, so pass \"-DLLVM_USE_LINKER=gold\" when configuring!")
-		set (LLVM_USE_LINKER "gold")
-	endif ()
-	execute_process(COMMAND ${CMAKE_C_COMPILER} -fuse-ld=gold -Wl,--version ERROR_QUIET OUTPUT_VARIABLE ld_version)
-	if ("${ld_version}" MATCHES "GNU gold")
-		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=gold -Wl,--disable-new-dtags")
-		set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fuse-ld=gold -Wl,--disable-new-dtags")
-	endif()
 endif ()
