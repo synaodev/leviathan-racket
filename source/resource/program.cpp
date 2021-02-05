61c40f84d1d63e9c341efb77cdabaf52dc8499be
Tyler Cadena <tylercadena@alum.calarts.edu>
2021-02-05 10:56:53 -0800

    nvidia now works

--- source/resource/program.cpp
+++ source/resource/program.cpp
@@ -4,7 +4,7 @@
 
 #include <fmt/format.h>
 
-static constexpr byte_t kMinorVert420[] = R"(#version 420 core
+static constexpr byte_t kMinorVert420[] = R"(
 layout(location = 0) in vec2 position;
 out STAGE {
 	layout(location = 0) vec4 color;
@@ -14,7 +14,7 @@ void main() {
 	vs.color = vec4(1.0f);
 })";
 
-static constexpr byte_t kMinorVert330[] = R"(#version 330 core
+static constexpr byte_t kMinorVert330[] = R"(
 layout(location = 0) in vec2 position;
 out STAGE {
 	vec4 color;
@@ -24,7 +24,7 @@ void main() {
 	vs.color = vec4(1.0f);
 })";
 
-static constexpr byte_t kBlankVert420[] = R"(#version 420 core
+static constexpr byte_t kBlankVert420[] = R"(
 layout(binding = 0, std140) uniform transforms {
 	mat4 viewports[2];
 };
@@ -39,7 +39,7 @@ void main() {
 	vs.color = color;
 })";
 
-static constexpr byte_t kBlankVert330[] = R"(#version 330 core
+static constexpr byte_t kBlankVert330[] = R"(
 layout(std140) uniform transforms {
 	mat4 viewports[2];
 };
@@ -54,7 +54,7 @@ void main() {
 	vs.color = color;
 })";
 
-static constexpr byte_t kMajorVert420[] = R"(#version 420 core
+static constexpr byte_t kMajorVert420[] = R"(
 layout(binding = 0, std140) uniform transforms {
 	mat4 viewports[2];
 };
@@ -78,7 +78,7 @@ void main() {
 	vs.palID = palID;
 })";
 
-static constexpr byte_t kMajorVert330[] = R"(#version 330 core
+static constexpr byte_t kMajorVert330[] = R"(
 layout(std140) uniform transforms {
 	mat4 viewports[2];
 };
@@ -102,7 +102,7 @@ void main() {
 	vs.palID = palID;
 })";
 
-static constexpr byte_t kFontsVert420[] = R"(#version 420 core
+static constexpr byte_t kFontsVert420[] = R"(
 layout(std140) uniform transforms {
 	mat4 viewports[2];
 };
@@ -125,7 +125,7 @@ void main() {
 	vs.table = table;
 })";
 
-static constexpr byte_t kFontsVert330[] = R"(#version 330 core
+static constexpr byte_t kFontsVert330[] = R"(
 layout(std140) uniform transforms {
 	mat4 viewports[2];
 };
@@ -148,7 +148,7 @@ void main() {
 	vs.table = table;
 })";
 
-static constexpr byte_t kColorsFrag420[] = R"(#version 420 core
+static constexpr byte_t kColorsFrag420[] = R"(
 in STAGE {
 	layout(location = 0) vec4 color;
 } fs;
@@ -157,7 +157,7 @@ void main() {
 	fragment = fs.color;
 })";
 
-static constexpr byte_t kColorsFrag330[] = R"(#version 330 core
+static constexpr byte_t kColorsFrag330[] = R"(
 in STAGE {
 	vec4 color;
 } fs;
@@ -166,7 +166,7 @@ void main() {
 	fragment = fs.color;
 })";
 
-static constexpr byte_t kSpritesFrag420[] = R"(#version 420 core
+static constexpr byte_t kSpritesFrag420[] = R"(
 layout(binding = 0) uniform sampler2DArray diffuse;
 in STAGE {
 	layout(location = 0) vec2 uvcoords;
@@ -180,7 +180,7 @@ void main() {
 	fragment = vec4(color.rgb, color.a * fs.alpha);
 })";
 
-static constexpr byte_t kSpritesFrag330[] = R"(#version 330 core
+static constexpr byte_t kSpritesFrag330[] = R"(
 uniform sampler2DArray diffuse;
 in STAGE {
 	vec2 uvcoords;
@@ -194,7 +194,7 @@ void main() {
 	fragment = vec4(color.rgb, color.a * fs.alpha);
 })";
 
-static constexpr byte_t kIndexedFrag420[] = R"(#version 420 core
+static constexpr byte_t kIndexedFrag420[] = R"(
 layout(binding = 0) uniform sampler2DArray diffuse;
 layout(binding = 1) uniform sampler1DArray palette;
 in STAGE {
@@ -210,7 +210,7 @@ void main() {
 	fragment = vec4(color.rgb, color.a * fs.alpha);
 })";
 
-static constexpr byte_t kIndexedFrag330[] = R"(#version 330 core
+static constexpr byte_t kIndexedFrag330[] = R"(
 uniform sampler2DArray diffuse;
 uniform sampler1DArray palette;
 in STAGE {
@@ -226,7 +226,7 @@ void main() {
 	fragment = vec4(color.rgb, color.a * fs.alpha);
 })";
 
-static constexpr byte_t kChannelsFrag420[] = R"(#version 420 core
+static constexpr byte_t kChannelsFrag420[] = R"(
 layout(binding = 2) uniform sampler2DArray channels;
 in STAGE {
 	layout(location = 0) vec2 uvcoords;
@@ -241,7 +241,7 @@ void main() {
 	fragment = color * fs.color;
 })";
 
-static constexpr byte_t kChannelsFrag330[] = R"(#version 330 core
+static constexpr byte_t kChannelsFrag330[] = R"(
 uniform sampler2DArray channels;
 in STAGE {
 	vec2 uvcoords;
@@ -259,57 +259,81 @@ void main() {
 namespace program {
 	std::string directive() {
 		return fmt::format(
-			"#version {}{}0",
+			"#version {}{}0 core\n",
 			opengl_version[0],
 			opengl_version[1]
 		);
 	}
 	std::string minor_vert() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kMinorVert420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kMinorVert420;
+		} else {
+			result += kMinorVert330;
 		}
-		return kMinorVert330;
+		return result;
 	}
 	std::string blank_vert() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kBlankVert420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kBlankVert420;
+		} else {
+			result += kBlankVert330;
 		}
-		return kBlankVert330;
+		return result;
 	}
 	std::string major_vert() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kMajorVert420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kMajorVert420;
+		} else {
+			result += kMajorVert330;
 		}
-		return kMajorVert330;
+		return result;
 	}
 	std::string fonts_vert() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kFontsVert420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kFontsVert420;
+		} else {
+			result += kFontsVert330;
 		}
-		return kFontsVert330;
+		return result;
 	}
 	std::string colors_frag() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kColorsFrag420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kColorsFrag420;
+		} else {
+			result += kColorsFrag330;
 		}
-		return kColorsFrag330;
+		return result;
 	}
 	std::string sprites_frag() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kSpritesFrag420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kSpritesFrag420;
+		} else {
+			result += kSpritesFrag330;
 		}
-		return kSpritesFrag330;
+		return result;
 	}
 	std::string indexed_frag() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kIndexedFrag420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kIndexedFrag420;
+		} else {
+			result += kIndexedFrag330;
 		}
-		return kIndexedFrag330;
+		return result;
 	}
 	std::string channels_frag() {
-		if (opengl_nvidia_card == false and opengl_version[0] == 4 and opengl_version[1] >= 2) {
-			return kChannelsFrag420;
+		std::string result = program::directive();
+		if (opengl_version[0] == 4 and opengl_version[1] >= 2) {
+			result += kChannelsFrag420;
+		} else {
+			result += kChannelsFrag330;
 		}
-		return kChannelsFrag330;
+		return result;
 	}
 }
