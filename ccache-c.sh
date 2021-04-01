e80bb2fcf45e66f1b54c9aa6dbe1cd54d57ac723
Tyler Cadena <tylercadena@alum.calarts.edu>
2021-03-31 21:15:01 -0700

    remove ccache scripts and don't use stdafx by default

--- ccache-c.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$1" = "${CMAKE_C_COMPILER}" ]; then
-    shift
-fi
-
-export CCACHE_CPP2=true
-exec "${C_LAUNCHER}" "${CMAKE_C_COMPILER}" "$@"
--- ccache-cxx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$1" = "${CMAKE_CXX_COMPILER}" ]; then
-    shift
-fi
-
-export CCACHE_CPP2=true
-exec "${CXX_LAUNCHER}" "${CMAKE_CXX_COMPILER}" "$@"
